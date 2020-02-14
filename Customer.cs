5a6672825851c8b098d0139809d7e094474f52fc
Matthew VH <vanheckematt@gmail.com>
2020-02-14 11:56:22 -0600

    Int variables added to customer class for thirst, satisfaction (which will change depending on comparison of taste constant and taste of today's lemonade). Likeliness to buy is calculated by multiplying thirst times satisfaction. Random object now gets passed into cunstomer class.

--- Customer.cs
+++ Customer.cs
@@ -8,5 +8,24 @@ namespace LemonadeStand_3DayStarter
 {
     class Customer
     {
+        //Member Variables
+        int thirst;
+        //Satisfaction will be added to or subtracted from depending on whether taste of lemonade is greater than, less than, or equal to tasteConstant.
+        int satisfaction;
+        int tasteConstant;
+        Random random;
+        //If likelinessToBuy is greater than variable (based on weather, popularity of stand, recipe, and price), customer will buy lemonade
+        int likelinessToBuy;
+        //Constructor
+        public Customer(Random random)
+        {
+            this.random = random;
+            tasteConstant = random.Next(1, 10);
+            thirst = random.Next(1, 3);
+            satisfaction = 1;
+            likelinessToBuy = thirst * satisfaction;
+        }
+        //Member Methods
+
     }
 }
