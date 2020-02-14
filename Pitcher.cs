1dccf9ee08fe29dfd41bcc59876af2a6ea43319e
Matthew VH <vanheckematt@gmail.com>
2020-02-14 15:03:06 -0600

    Pitcher class filled out int variables for number of cups remaining and taste. These will be taken in as parameters to the constructor based on the recipe. Pitcher also has a pour cup method which will subtract one from number of cups remaining each time method is called. Next goal: look at recipe class and figure out how it interacts with the inventory as well as how a pitcher will be based on it.

--- Pitcher.cs
+++ Pitcher.cs
@@ -8,5 +8,19 @@ namespace LemonadeStand_3DayStarter
 {
     class Pitcher
     {
+        //Member Variables
+        public int numberOfCupsRemaining;
+        public int taste;
+        //Constructor
+        public Pitcher(int numberOfCups, int taste)
+        {
+            this.numberOfCupsRemaining = numberOfCups;
+            this.taste = taste;
+        }
+        //Member Methods
+        public void PourCup()
+        {
+            numberOfCupsRemaining--;
+        }
     }
 }
