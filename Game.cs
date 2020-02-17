d1ab734095608674b005ea4df1fe9270f7485357
Matthew VH <vanheckematt@gmail.com>
2020-02-17 09:23:57 -0600

    Basic outline for PlayGame method in Game class laid out. Next goal: add variables and methods neccessary to keeping track of income and expenses to wallet.

--- Game.cs
+++ Game.cs
@@ -23,6 +23,19 @@ namespace LemonadeStand_3DayStarter
             currentDay = 0;
         }
         //Member Methods
-
+        public void PlayGame()
+        {
+            //Loop through for each day.
+                //Display day, weather, current inventory, and amount of money in wallet.
+                //Give player the opportunity to purchase items from the store.
+                //Give player the opportunity to choose the recipe for their lemonade, and how much of it they would like to make.
+                //Begin work day.
+                    //Have customers walk by one at a time and, if the math works out, have the customer buy.
+                //At the end of the day, print how many cups were sold out of how many possible.
+                //Print remaining inventory.
+                //Maybe give some sort of popularity indicator.
+            //Repeat for next day.
+            //At the end of the game, print overall stats (at least profit/loss, but maybe also total amount spent on ingredients)
+        }
     }
 }
