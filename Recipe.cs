69c63c90e9e14d41b0ab9f2338c91c463712440c
Matthew VH <vanheckematt@gmail.com>
2020-02-19 16:27:30 -0600

    Refactored AdjustRecipe method in Recipe class so that each ingredient is dealt with in its own method. Refactored WouldYouLikeToAdjustRecipe, splitting parts off into GetStringUserInput and CheckIfUserResponseValid. Next goal: Keep refactoring methods.

--- Recipe.cs
+++ Recipe.cs
@@ -44,15 +44,31 @@ namespace LemonadeStand_3DayStarter
         public void AdjustRecipe(Inventory inventory)
         {
             Console.WriteLine("Note: if the selected quantity of an item is greater than the amount you have left in your inventory, the rest of that item from your inventory will go into the batch.\n");
+            SetSizeOfBatch(inventory);
+            SetNumberOfLemons(inventory);
+            SetAmountOfSugar(inventory);
+            SetNumberOfIceCubes(inventory);
+        }
+        public void SetSizeOfBatch(Inventory inventory)
+        {
             Console.WriteLine("Current size of recipe: " + numberOfCups + " cups.\nYou currently have " + inventory.Cups.Count + " cups available to use.");
             Console.WriteLine("How big would you like to make it?");
             numberOfCups = GetValidIntFromUserInput();
+        }
+        public void SetNumberOfLemons(Inventory inventory)
+        {
             Console.WriteLine("Current number of lemons in recipe: " + numberOfLemons + "\nYou currently have " + inventory.Lemons.Count + " lemons available to use.");
             Console.WriteLine("How many would you like to include in your recipe?");
             numberOfLemons = GetValidIntFromUserInput();
+        }
+        public void SetAmountOfSugar(Inventory inventory)
+        {
             Console.WriteLine("Current quantity of sugar in recipe: " + numberOfSugarCubes + " cubes.\nYou currently have " + inventory.SugarCubes.Count + " sugar cubes available to use.");
             Console.WriteLine("How many would you like to include?");
             numberOfSugarCubes = GetValidIntFromUserInput();
+        }
+        public void SetNumberOfIceCubes(Inventory inventory)
+        {
             Console.WriteLine("Current number of ice cubes in recipe: " + numberOfIceCubes + "\nYou currently have " + inventory.IceCubes.Count + " available to use.");
             Console.WriteLine("How many would you like to include?");
             numberOfIceCubes = GetValidIntFromUserInput();
@@ -66,7 +82,7 @@ namespace LemonadeStand_3DayStarter
             {
                 if (!validResponse || chosenNumber < 0)
                 {
-                    Console.WriteLine("Invalid response. Please type a non-negative integer");
+                    Console.WriteLine("Invalid response. Please type a non-negative integer.");
                 }
                 validResponse = int.TryParse(Console.ReadLine(), out chosenNumber);
             } while (!validResponse || chosenNumber < 0);
@@ -82,12 +98,21 @@ namespace LemonadeStand_3DayStarter
             string response = "";
             do
             {
-                response = Console.ReadLine();
-                if (response.ToLower() != "yes" && response.ToLower() != "no")
-                {
-                    Console.WriteLine("Invalid response. Please type either \"yes\" or \"no\".");
-                }
+                response = GetStringUserInput();
             } while (response.ToLower() != "yes" && response.ToLower() != "no");
+            return CheckIfUserResponseValid(response);
+        }
+        public string GetStringUserInput()
+        {
+            string userIn = Console.ReadLine();
+            if (userIn.ToLower() != "yes" && userIn.ToLower() != "no")
+            {
+                Console.WriteLine("Invalid response. Please type either \"yes\" or \"no\".");
+            }
+            return userIn;
+        }
+        public bool CheckIfUserResponseValid(string response)
+        {
             if (response.ToLower() == "yes")
             {
                 return true;
